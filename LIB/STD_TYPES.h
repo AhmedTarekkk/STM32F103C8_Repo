8e08f6d6f2cf290a9ef46b044b274c869d2c9a3d
Ahmed Tarek <ahmed.tarek55525@gmail.com>
2023-03-07 05:43:50 +0200

    Add Library Layer

---
+++ LIB/STD_TYPES.h
@@ -0,0 +1,19 @@
+#ifndef STD_TYPES_H
+#define STD_TYPES_H
+
+typedef unsigned char       u8;
+typedef signed char         s8;
+typedef unsigned short int u16;
+typedef signed short int   s16;
+typedef unsigned long int  u32;
+typedef signed long int    s32;
+typedef float              f32;
+typedef double             f64;
+typedef long double        f128;
+
+#define STD_TYPES_OK            1
+#define STD_TYPES_NOK           0
+
+#define NULL                   ((void*)0)
+
+#endif
---
+++ LIB/stm32f103xx.h
@@ -0,0 +1,32 @@
+/******************************************************************************
+*  File name:		stm32f103xx.h
+*  Author:			Mar 5, 2023
+*  Author:			Ahmed Tarek
+*  Version:         1.0
+*******************************************************************************/
+#ifndef STM32F103xx_H
+#define STM32F103xx_H
+
+/*******************************************************************************
+*                      				RCC                     		           *
+*******************************************************************************/
+
+#define RCC_u32_BASE_ADDRESS						0x40021000
+
+typedef struct
+{	
+	volatile u32 CR;
+	volatile u32 CFGR;
+	volatile u32 CIR;
+	volatile u32 APB2RSTR;
+	volatile u32 APB1RSTR;
+	volatile u32 AHBENR;
+	volatile u32 APB2ENR;
+	volatile u32 APB1ENR;
+	volatile u32 BDCR;		
+	volatile u32 CSR;				
+}RCC_RegDef_t;
+
+#define RCC			((RCC_RegDef_t *)RCC_u32_BASE_ADDRESS)
+
+#endif
